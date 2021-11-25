63715b99dec9a4d2448f63ca5b7b5120afc78478
Joseph Bassey <jbbliss9@gmail.com>
2021-11-25 05:06:33 +0100

    added a few changes

---
+++ app.js
@@ -0,0 +1,23 @@
+const JournalData = {
+	data() {
+		return {
+			message: ''
+		}
+	},
+
+	methods: {
+		input
+	},
+}
+
+const CopyRight = {
+	data() {
+		return {
+			message: 'Academic Hive 2021 Powered by Elseiver'
+		}
+	}
+}
+
+
+// Vue.createApp(JournalData).mount('.search-holder');
+Vue.createApp(CopyRight).mount('.copyright');
