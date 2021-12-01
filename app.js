f0eb0fdb9f17cd7d1034273c7ab158ebe5c98e6b
Joseph Bassey <jbbliss9@gmail.com>
2021-12-01 06:55:07 +0100

    added a test api

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const JournalData = {
                 searchTerm : null
             },
             searchResults : false,
+            totalResults: [],
+  			posts : [],
             searchOutput: []
 		}
 	},
@@ -15,11 +17,18 @@ const JournalData = {
         	e.preventDefault(),
             // $(".searcharea").hide(),
             this.searchResults = !this.searchResults,
+            // axios
+            // .get('https://api.elsevier.com/content/search/sciencedirect?query=' + this.requests.searchTerm + '&apiKey=7f59af901d2d86f78a1fd60c1bf9426a')
+            // .then(response => (
+            // 	console.log(response.data),
+            // 	this.totalResults = response.data["search-results"]["opensearch:totalResults"],
+            // 	this.authors = response.data
+            // ));
             axios
-            .get('https://api.elsevier.com/content/search/sciencedirect?query=' + this.requests.searchTerm + '&apiKey=7f59af901d2d86f78a1fd60c1bf9426a')
+            .get('https://jsonplaceholder.typicode.com/posts')
             .then(response => (
-            	console.log(response.data),
-            	this.searchOutput = response.data
+             console.log(response.data),
+             this.posts = response.data
             ));
         }
 	}
